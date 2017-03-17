782e9126ab541d153e7dab68e64baa373a746999
Yiming-Shen <simsept@163.com>
2017-03-17 21:23:25 +0000

    edit calculation

--- httpcodes.py
+++ httpcodes.py
@@ -5,6 +5,7 @@ Due to changes (httplib to http.<>) to get the codes, they are wrapped into
 httpcodes with an import of the underlying library dependent on python version.
 """
 import sys
+
 if sys.version_info[0] == 2:
     import httplib as httpcodes
 else:
